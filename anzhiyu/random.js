03b5a1037fec78ac6d0ef98102c85eb96577ed89
sonjayce <3489073251@qq.com>
2025-07-05 00:21:53 +0800

    Deploy: 547e76713fb676c1a9dab3d92349f23ec3bdb569 sonjayce/sonjayce.github.io-source@547e76713fb676c1a9dab3d92349f23ec3bdb569

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/05/hello-world/","2025/07/04/test/","2025/07/04/test1/","2025/07/04/test2/"];function toRandomPost(){
+var posts=["2025/07/05/hello-world/","2025/07/04/test/","2025/07/04/test1/","2025/07/04/test2/","2025/07/05/test3/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
